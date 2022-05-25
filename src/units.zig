a5ef8bd066c258c074f5d3bf06c9806298f138c4
Mitch Kennedy <mitchk@runbox.com>
2022-05-26 08:50:48 +1000

    adding some units

--- src/units.zig
+++ src/units.zig
@@ -8,32 +8,35 @@ const UnitDimension = struct {
     Luminous: isize = 0,
 };
 
-const ConversionRate = struct {
-    ratio: f64,
-    baseUnit: *const UnitDefinition,
-};
-
-const UnitDefiniton = struct {
+pub const UnitRatio = struct {
     name: []const u8,
+    ratio: f64,
+};
+
+pub const UnitsDefinition = struct {
     dimension: UnitDimension,
-    conversion: ?ConversionRate,
+    baseUnitName: []const u8,
+    unitRatio: []const UnitRatio,
 };
 
-pub const Metre = UnitDefiniton{
-    .name = "Metre",
-    .dimension = .{
-        .Length = 1,
+const LengthUnits = UnitsDefinition{
+    .dimension = .{ .length = 1 },
+    .baseUnitName = "Metre",
+    .unitRatio = &.{
+        .{ .name = "KiloMetre", .ratio = 1000 },
+        .{ .name = "Foot", .ratio = 0.3048 },
+        .{ .name = "Inch", .ratio = 0.0254 },
+        .{ .name = "Yard", .ratio = 0.9144 },
+        .{ .name = "Mile", .ratio = 1.609344 },
     },
-    .conversion = null,
 };
 
-pub const Yard = UnitDefinition{
-    .name = "Yard",
-    .dimension = .{
-        .Length = 1,
-    },
-    .conversion = .{
-        .ratio = 0.9144,
-        .baseUnit = &Metre,
+const AreaUnits = UnitsDefinition{
+    .dimension = .{ .length = 2 },
+    .baseUnitName = "SquareMetre",
+    .unitRatio = &.{
+        .{ .name = "acre", .ratio = 4046.873 },
+        .{ .name = "Hectare", .ratio = 10000 },
+        .{ .name = "SquareInch", .ratio = 0.0064516 },
     },
 };
